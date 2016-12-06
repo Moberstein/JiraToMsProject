052a9730191dc9d8ab6c6de16873aae0efc37fe1
Micha Oberstein <benj.konto@t-online.de>
2016-12-06 19:18:44 +0100

    Add Jira logo to Resources
    Add Button on Ribbon Bar

--- JiraToMsProject/JiraRibbon.Designer.cs
+++ JiraToMsProject/JiraRibbon.Designer.cs
@@ -35,21 +35,33 @@
         private void InitializeComponent()
         {
             this.tab1 = this.Factory.CreateRibbonTab();
-            this.group1 = this.Factory.CreateRibbonGroup();
+            this.group_jira = this.Factory.CreateRibbonGroup();
+            this.button_import_jira = this.Factory.CreateRibbonButton();
             this.tab1.SuspendLayout();
+            this.group_jira.SuspendLayout();
             this.SuspendLayout();
             // 
             // tab1
             // 
             this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
-            this.tab1.Groups.Add(this.group1);
+            this.tab1.Groups.Add(this.group_jira);
             this.tab1.Label = "TabAddIns";
             this.tab1.Name = "tab1";
             // 
-            // group1
+            // group_jira
             // 
-            this.group1.Label = "group1";
-            this.group1.Name = "group1";
+            this.group_jira.Items.Add(this.button_import_jira);
+            this.group_jira.Label = "Jira";
+            this.group_jira.Name = "group_jira";
+            // 
+            // button_import_jira
+            // 
+            this.button_import_jira.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button_import_jira.Description = "Import Jira Tasks as XSL";
+            this.button_import_jira.Image = global::JiraToMsProject.Properties.Resources.jira_logo;
+            this.button_import_jira.Label = "Import Jira XSL";
+            this.button_import_jira.Name = "button_import_jira";
+            this.button_import_jira.ShowImage = true;
             // 
             // JiraRibbon
             // 
@@ -59,6 +71,8 @@
             this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.JiraRibbon_Load);
             this.tab1.ResumeLayout(false);
             this.tab1.PerformLayout();
+            this.group_jira.ResumeLayout(false);
+            this.group_jira.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -66,7 +80,8 @@
         #endregion
 
         internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
-        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group_jira;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button_import_jira;
     }
 
     partial class ThisRibbonCollection
