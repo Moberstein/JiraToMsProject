ffd91b5a85d66a9c9361a698c6a9033c79a6f1d0
Micha Oberstein <benj.konto@t-online.de>
2016-12-06 19:03:50 +0100

    Create empty Jira Ribbon

---
+++ JiraToMsProject/JiraRibbon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace JiraToMsProject
+{
+    public partial class JiraRibbon
+    {
+        private void JiraRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+    }
+}
