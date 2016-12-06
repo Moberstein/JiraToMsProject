79de6c525adb1e1c890b29aadb98c75e6568dce4
Micha Oberstein <benj.konto@t-online.de>
2016-12-06 20:18:55 +0100

    Add new Class Issue

---
+++ JiraToMsProject/Issue.cs
@@ -0,0 +1,17 @@
+﻿namespace JiraToMsProject
+{
+    class Issue
+    {
+        public string Name { get; set; }
+        public string Ressource { get; set; }
+        public string[] Sprints { get; set; }
+        public string Key { get; set; }
+        public string Type { get; set; }
+        public string Estimated { get; set; }
+        public string SubTasks { get; set; }
+        public string Linked { get; set; }
+        public string[] Labels { get; set; }
+        public object Epic { get; set; }
+        public string Created { get; set; }
+    }
+}
